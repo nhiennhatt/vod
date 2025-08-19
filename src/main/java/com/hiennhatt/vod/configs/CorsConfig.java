625f910e6e5ec5d47c78a46caecd3f36d78bd013
nhiennhatt <hiennhatt1804@gmail.com>
2025-08-19 17:16:12 +0700

    chore: add allow origins

---
+++ src/main/java/com/hiennhatt/vod/configs/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.hiennhatt.vod.configs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootConfiguration
+public class CorsConfig {
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowCredentials(true).allowedOrigins(env.getProperty("allowedOrigin", "*")).allowedMethods("*");
+            }
+        };
+    }
+}
